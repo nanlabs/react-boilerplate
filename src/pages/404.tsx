7996b3377264f3149e37d14a54e2f2b2220d2640
Roberto Molina <roberto.molina@nan-labs.com>
2022-06-22 23:23:31 -0300

    add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const NotFound: FC = () => {
+  return <div>404</div>;
+};
+
+export default NotFound;
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -6,6 +6,7 @@ import { Routes as RouteMap } from './Routes';
 import UserDetail from 'pages/UserDetail';
 import LinksExamples from 'pages/Links';
 import Powered from 'pages/Powered';
+import NotFound from 'pages/404';
 
 const AppRoutes: FC = () => (
   <Routes>
@@ -13,6 +14,7 @@ const AppRoutes: FC = () => (
     <Route path={RouteMap.POWERED} element={<Powered />} />
     <Route path={RouteMap.LINKS_EXAMPLES} element={<LinksExamples />} />
     <Route path={RouteMap.USER_DETAIL} element={<UserDetail />} />
+    <Route path="*" element={<NotFound />} />
   </Routes>
 );
 
