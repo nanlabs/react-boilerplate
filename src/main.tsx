13ef500d9635bff97a71a110dba0ba863b006217
ulises-jeremias <ulisescf.24@gmail.com>
2023-02-15 04:45:41 -0300

    Finished setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import '@/i18n';
-import * as serviceWorker from '@/service-worker';
 import reportWebVitals from '@/report-web-vitals';
 import App from '@/App';
 
@@ -17,8 +16,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
